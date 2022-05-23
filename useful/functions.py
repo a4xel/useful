e19d49e471004f2edc9a6cb271af4a444bcd1c3b
Axel Lechanteur <lechanteuraxel@gmail.com>
2022-05-23 13:53:12 +0200

    test push

---
+++ useful/functions.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+def plot_history(history):
+    plt.plot(history.history['loss'])
+    plt.title('Train loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.show()
+
+def hello(name):
+    print(f"Hello{name}")
